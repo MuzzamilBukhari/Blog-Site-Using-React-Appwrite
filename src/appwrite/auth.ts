78c141be7a42544308e6ee6b3ad96216497dc57b
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-07-05 13:13:36 +0500

    adding files

---
+++ src/appwrite/auth.ts
@@ -0,0 +1,50 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+class AuthService {
+  client = new Client();
+  account: Account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteProjectUrl)
+      .setProject(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  createAccount = async ({
+    email,
+    password,
+    name,
+  }: {
+    email: string;
+    password: string;
+    name: string;
+  }) => {
+    try {
+      return await this.account.create(ID.unique(), email, password, name);
+    } catch (error) {
+      console.log("Error in auth service : ", error);
+    }
+  };
+
+  login = async ({ email, password }: { email: string; password: string }) => {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      console.log("Error in auth service : ", error);
+    }
+  };
+
+  logout = async () => {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Error in auth service : ", error);
+    }
+  };
+}
+
+const authService = new AuthService();
+
+export default authService;
