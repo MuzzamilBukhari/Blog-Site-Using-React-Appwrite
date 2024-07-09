fa2d5e0e5fe20a0752a0016505ac0b6dbff82f23
Muhammad Muzzamil Bukhari <muhammadmuzzmilbukhari@gmail.com>
2024-07-09 10:31:25 +0500

    add slug

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,12 +6,16 @@ const Input = (
     type,
     className = "",
     placeholder = "",
+    readonly = false,
+    onInput,
     ...props
   }: {
     label: string;
     type: string;
     className?: string;
     placeholder: string;
+    readonly?: boolean;
+    onInput?: (e: any) => void;
   },
   ref: LegacyRef<HTMLInputElement>
 ) => {
@@ -29,6 +33,8 @@ const Input = (
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         id={id}
+        onInput={onInput}
+        readOnly={readonly}
         {...props}
       />
     </div>
--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -4,11 +4,12 @@ import databaseServices from "../appwrite/database";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { createPost } from "../store/postsSlice";
 
 interface PostFormData {
   title: string;
+  slug: string;
   content: string;
   status: string;
 }
@@ -18,7 +19,7 @@ const PostForm = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userData = useSelector((state: RootState) => state.auth.userData);
-  const { register, handleSubmit } = useForm<PostFormData>();
+  const { register, handleSubmit, watch, setValue } = useForm<PostFormData>();
 
   const submit = async (data: PostFormData) => {
     setError("");
@@ -27,13 +28,37 @@ const PostForm = () => {
 
       const postData = await databaseServices.createPost(data, userData?.$id);
       dispatch(createPost(postData));
-      navigate("/");
+      navigate(`/post/${postData.$id}`);
     } catch (error) {
       if (error instanceof Error) setError(error.message);
       throw error;
     }
   };
 
+  const slugTransform = useCallback((value: string) => {
+    if (value) {
+      return value
+        .trim()
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, "-")
+        .replace(/^-+|-+$/g, "");
+    }
+    return "";
+  }, []);
+
+  useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === "title") {
+        if (value.title)
+          setValue("slug", slugTransform(value.title), {
+            shouldValidate: true,
+          });
+      }
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [watch, slugTransform, setValue]);
   return (
     <div>
       <p>{error}</p>
@@ -43,6 +68,14 @@ const PostForm = () => {
           type="text"
           placeholder="Enter post title"
           {...register("title", { required: true })}
+        />
+        <Input
+          label="Slug : "
+          type="text"
+          placeholder="slug"
+          {...register("slug", { required: true })}
+          readonly={true}
+          onInput={(e) => setValue("slug", slugTransform(e.target.value))}
         />
         <Input
           label="Post description : "
